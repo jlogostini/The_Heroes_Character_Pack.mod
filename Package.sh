7a93c25a2000efeb415acaaf1a7e6af7f0547d18
John Logostini <johnlogostini16485@gmail.com>
2022-11-18 09:46:10 -0400

    Fixed the sapces in the package name

--- Package.sh
+++ Package.sh
@@ -4,8 +4,8 @@ cp -r assets/ .temp-986286
 cp -r resources/ .temp-986286
 cp config.json .temp-986286
 cd .temp-986286
-zip -r The Heroes Character Pack.mod assets/ resources/ config.json
+zip -r The_Heroes_Character_Pack.mod assets/ resources/ config.json
 cd ../
 mkdir Package
-mv .temp-986286/The Heroes Character Pack.mod Package/
+mv .temp-986286/The_Heroes_Character_Pack.mod Package/
 rm -Rf .temp-986286
